f19f8816d9958c58acb9bca8b4f2938e2d6fa023
Vbrand01 <viccorin20@gmail.com>
2023-02-06 10:19:12 -0300

    upload

---
+++ robo.py
@@ -0,0 +1,13 @@
+from mouseinfo import mouseInfo
+import pyautogui
+
+mouseInfo
+# pyautogui.click(974,374, duration = 2)
+# pyautogui.click(104,103, duration = 2)
+
+pyautogui.click(539,467, duration = 2)
+pyautogui.click(705,620, duration = 2)
+
+pyautogui.click(705,620, duration = 2)
+pyautogui.click(705,620, duration = 2)
+pyautogui.click(651,633, duration = 2)
